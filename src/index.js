2eaac925afada81ef32c367b697946758eb845c2
whattablackhole <rudolf.gritsenyak@gmail.com>
2020-10-25 23:33:44 +0300

    feat: add code

--- src/index.js
+++ src/index.js
@@ -1,6 +1,24 @@
 
-// You should implement your task here.
-
 module.exports = function towelSort (matrix) {
-  return [];
+  if(matrix === undefined){
+    return []
+  }
+  let flattened
+  if (Array.isArray(matrix[0])){
+    flattened = matrix.reduce((acc, curVal,ind) => {
+      if (ind%2==0){
+        return acc.concat(curVal)
+      }
+      else {
+        return acc.concat(curVal.reverse())
+      }
+      
+      }, []);
+      
+  }
+  else { 
+    return []
+  }
+  
+ return flattened 
 }
